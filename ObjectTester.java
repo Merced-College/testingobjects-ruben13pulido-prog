573a27fc586142baf157318429983f13f5e82b23
ruben13pulido-prog <ruben13pulido@gmail.com>
2025-09-12 00:24:53 +0000

    Submission

--- ObjectTester.java
+++ ObjectTester.java
@@ -2,14 +2,15 @@ public class ObjectTester {
        
         public static void main (String[] args) {
 
-            //make a Dog object - instantiation 
-            Dog dog1 = new Dog();
-            System.err.println(dog1);
-
-            //make a Dog object - use non-default constructor
-            Dog dog2 = new Dog("Karl", 3, "golden retriever");
-            
-             System.err.println(dog2);
+           // create Pizza objects using both constructors
+                  Pizza pizza1 = new Pizza();
+                  System.err.println(pizza1);
+                 
+                  Pizza pizza2 = new Pizza ("Pepperoni", "Cheesy", 4);
+                  System.err.println(pizza2);
+                 
+                  Pizza pizza3 = new Pizza ("Hawaiian", "Pineapple", 6);
+                  System.err.println(pizza3);
 
         } //end main 
           
--- Pizza.java
+++ Pizza.java
@@ -1,6 +1,51 @@
-public class Pizza {
+// Ruben Pulido 
+// 9/9/25
+// Pizza class 
 
-    
-        
+    public class Pizza {
+        private String pizza;
+        private int slices;
+        private String flavor;
 
-}
+        // default constructor 
+          public Pizza() {
+            this.pizza = "cheese";
+            this.flavor = "pepperoni";
+            this.slices = 8;
+          }
+
+          // parameterized constructor 
+            public Pizza(String pizza, String flavor, int slices) {
+                this.pizza = pizza;
+                this.flavor = flavor;
+                this.slices = slices;
+            }
+
+         // setters
+            public void setPizza(String pizza) {
+                this.pizza = pizza;
+            }
+            public void setFlavor(String flavor) {
+                this.flavor = flavor;
+            }
+            public void setSlices(int slices) {
+                this.slices = slices;
+            }
+
+            // getters 
+            public String getPizza() {
+                return pizza;
+            }
+
+            public String getFlavor() {
+                return flavor;
+            }
+            public int getSlices() {
+                return slices;
+            }
+            @Override 
+              public String toString() {
+                return pizza + "," + flavor + "," + slices;
+                
+              }
+} // end Pizza class 
